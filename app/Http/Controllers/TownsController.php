eb5c3538f5a216ed81f9fbbcaa00487800c22133
Tonny <musembitonnyj@gmail.com>
2022-01-13 11:26:17 +0300

    add

---
+++ app/Http/Controllers/TownsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Resources\TownsResource;
+use App\Models\towns;
+use Illuminate\Http\Request;
+
+class TownsController extends Controller
+{
+    public function index(){
+        return new TownsResource(towns::all());
+    }
+}
