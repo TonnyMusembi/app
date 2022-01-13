b02aaa243081a1d752a2e956219054164aae97d2
Tonny <musembitonnyj@gmail.com>
2022-01-13 14:50:01 +0300

    add

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Resources\RegisterResource;
+use App\Models\register;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+  public function index(){
+        return new RegisterResource(register::all());
+    }
+}
